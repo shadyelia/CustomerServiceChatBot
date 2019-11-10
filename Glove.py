1994a8cee1149bc71ef356068e2d3f7dedc58724
ShadyElia <shady.elia@hotmail.com>
2019-11-10 16:22:14 +0200

    add some intents and start building glove

---
+++ Glove.py
@@ -0,0 +1,16 @@
+# importing the glove library
+from glove import Corpus, Glove
+
+
+def glove(lines):
+    # creating a corpus object
+    corpus = Corpus()
+    # training the corpus to generate the co occurence matrix which is used in GloVe
+    corpus.fit(lines, window=10)
+    # creating a Glove object which will use the matrix created in the above lines to create embeddings
+    # We can set the learning rate as it uses Gradient Descent and number of components
+    glove = Glove(no_components=5, learning_rate=0.05)
+
+    glove.fit(corpus.matrix, epochs=30, no_threads=4, verbose=True)
+    glove.add_dictionary(corpus.dictionary)
+    glove.save('glove.model')
